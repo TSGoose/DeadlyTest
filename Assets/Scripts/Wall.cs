e6f71ba4b7ae7600d74c4b378826245ea040942e
TSGoose <mrprokst@gmail.com>
2023-07-13 15:21:56 +0400

    bagfix

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -6,11 +6,11 @@ public class Wall : MonoBehaviour
 {
     void OnTriggerEnter2D(Collider2D collision)
     {
-    // Действия при столкновении с объектом
-    Debug.Log("Столкновение с объектом: " + collision.gameObject.name);
+        // Действия при столкновении с объектом
+        Debug.Log("Столкновение с объектом: " + collision.gameObject.name);
 
-    // Уничтожение пули после столкновения
-    Destroy(gameObject);
+        // Уничтожение пули после столкновения
+        Destroy(gameObject);
     }
 }
 
